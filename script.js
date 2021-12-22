db9907900ead110efb7d05ede637cd9b3ee27e48
ragujan <84169945+ragujan@users.noreply.github.com>
2021-12-22 16:33:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-var HT = document.getElementById('headerText');
+var nBar = document.getElementById('navBar');
+var nIcon = document.getElementById('navIcon');
+var fCDiv = document.getElementById('fourthDiv');
+
+nIcon.addEventListener('click', () => {
+
+
+        nBar.classList.toggle('d-none');
+        nIcon.classList.toggle('changeColor');
+    })
+    /*
+    window.addEventListener('resize', () => {
+        if (window.innerWidth >= 578) {
+
+            nBar.classList.toggle("navbarDropdown");
+
+
+        }
+    })*/
